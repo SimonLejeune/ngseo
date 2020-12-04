91eb7d4c05a34e2a07947f8520d41010a848df9a
harliard <nicolas.duval@epitech.eu>
2020-12-04 02:34:52 +0100

    feat(subscribe): redirec to profile

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -26,7 +26,7 @@ export class NotificationComponent implements OnInit {
       }))
       .catch(err => console.error('Could not subscribe to notifications', err));
     this.swPush.notificationClicks.subscribe((result) => {
-      window.open('https://google.com/', '_blank');
+      window.open('https://ngseo.netlify.app/profile', '_blank');
       console.log('clicked', result);
     });
   }
